8e86fbe28f9b49884d4ed59aaa9e3fb41ef5e66a
album <november1943@outlook.com>
2016-09-02 10:26:54 +0800

    second commit

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     FLASKY_MAIAL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = '710162464@qq.com'
-    FLASKY_ADMIN = '710162464@qq.com'
+    FLASKY_MAIL_SENDER = 'xxxxx'
+    FLASKY_ADMIN = 'xxxxxx'
     FLASKY_POSTS_PER_PAGE = 15
     FLASKY_FOLLOWERS_PER_PAGE = 15
     FLASKY_COMMENTS_PER_PAGE = 10
@@ -26,8 +26,8 @@ class DevelopmentConfig(Config):
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = '710162464'
-    MAIL_PASSWORD = 'syidwcbunrxqbcee'
+    MAIL_USERNAME = 'xxxxxx' # qq号
+    MAIL_PASSWORD = 'sxxxxx' # qq邮箱第三方授权码
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
